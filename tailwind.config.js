d2a8dd24f812764d31cbd7a60cd700085fbdd1ea
harshmishra366 <mishraharsh5959@gmail.com>
2024-07-21 22:23:03 +0530

    added new componets

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./**/*.html"],
+  theme: {
+    extend: {
+     backgroundImage :{
+      'img':"url('https://freepngimg.com/thumb/school/34071-8-students-learning.png')"
+     }
+    },
+  },
+  plugins: [],
+}
+
